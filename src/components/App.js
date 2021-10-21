f897a7ecfee365bb608eff7e7c9ee8d62ac4190b
ivanklak <ivanklak17@gmail.com>
2021-10-21 14:56:52 +0300

    refactor: Creating App, Downloading the Server Code, Queries(Loading Links), Mutations(Creating Links), Routing, Authentication, Updating the Store

---
+++ src/components/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+
+import CreateLink from './CreateLink';
+import Header from './Header';
+import LinkList from './LinkList';
+import Login from "./Login";
+
+const App = () => {
+    return (
+        <div className="center w85">
+            <Header/>
+            <div className="ph3 pv1 background-gray">
+                <Switch>
+                    <Route exact path="/" component={LinkList}/>
+                    <Route
+                        exact
+                        path="/create"
+                        component={CreateLink}
+                    />
+                    <Route exact path="/login" component={Login}/>
+                </Switch>
+            </div>
+        </div>
+    );
+};
+
+export default App;
---
+++ src/components/LinkList.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import Link from './Link';
+import {useQuery, gql} from '@apollo/client';
+
+export const FEED_QUERY = gql`
+    {
+        feed {
+            id
+            links {
+                id
+                createdAt
+                url
+                description
+                postedBy {
+                    id
+                    name
+                }
+                votes {
+                    id
+                    user {
+                        id
+                    }
+                }
+            }
+        }
+    }
+`;
+
+const LinkList = () => {
+    const {data} = useQuery(FEED_QUERY);
+
+    return (
+        <div>
+            {data && (
+                <>
+                    {data.feed.links.map((link, index) => (
+                        <Link key={link.id} link={link} index={index}/>
+                    ))}
+                </>
+            )}
+        </div>
+    );
+};
+
+export default LinkList;
